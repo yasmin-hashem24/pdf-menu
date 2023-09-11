a8cabc103c7a0dd57c3ac1ebd14ce10096ad2a42
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-09-11 06:25:14 +0300

    responsiveness for restaurant pages and headers for all normal

---
+++ PDFMenu/PDFMenu/Pages/Features/NormalUsers/ContactUs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PDFMenu.Pages.Features.NormalUsers
+{
+    public class ContactUsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
