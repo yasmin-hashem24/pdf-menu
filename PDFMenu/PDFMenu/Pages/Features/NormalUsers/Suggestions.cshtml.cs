fabdba50d7f02c2f2a6dc59d437fee576b8c316a
yasminmessycodes <84807559+yasmin-hashem24@users.noreply.github.com>
2023-09-20 19:40:54 +0300

    suggestions page , some bugs

--- PDFMenu/PDFMenu/Pages/Features/NormalUsers/Suggestions.cshtml.cs
+++ PDFMenu/PDFMenu/Pages/Features/NormalUsers/Suggestions.cshtml.cs
@@ -9,6 +9,7 @@ namespace PDFMenu.Pages.Features.NormalUsers;
 public class SuggestionsModel : PageModel
 {
     private readonly EdgeDBClient _edgeDbClient;
+    public string City;
     public List<RestaurantGot> Restaurants { get; set; }
     public SuggestionsModel(EdgeDBClient edgeDbClient)
     {
@@ -18,15 +19,28 @@ public class SuggestionsModel : PageModel
     {
         string partialNameC = name.ToLower();
         string partialNameS = name.ToUpper();
-        var query = "SELECT restaurant { opening_hours, menu_upload_date, menu_pdf, email, tags, password, restaurant, main_photo, phone_number, cover_photo, facebook, instagram, twitter, country, address, city, district, rating } " +
-             "FILTER restaurant.city = <str>$city AND ( restaurant.restaurant LIKE '%' ++ <str>$partialNameC ++ '%' OR  restaurant.restaurant LIKE '%' ++ <str>$partialNameS ++ '%');";
+        string partialNameT = char.ToUpper(name[0]) + name.Substring(1);
+        Console.WriteLine(partialNameT);
+        City = city;
+        var query = "";
+        if (city != "All Cities")
+        {
+            query = "SELECT restaurant { opening_hours, menu_upload_date, menu_pdf, email, tags, password, restaurant, main_photo, phone_number, cover_photo, facebook, instagram, twitter, country, address, city, district, rating } " +
+                 "FILTER restaurant.city = <str>$city AND ( restaurant.restaurant LIKE '%' ++ <str>$partialNameT ++ '%' OR restaurant.restaurant LIKE '%' ++ <str>$partialNameC ++ '%' OR  restaurant.restaurant LIKE '%' ++ <str>$partialNameS ++ '%');";
+        }
+        else
+        {
+            query = "SELECT restaurant { opening_hours, menu_upload_date, menu_pdf, email, tags, password, restaurant, main_photo, phone_number, cover_photo, facebook, instagram, twitter, country, address, city, district, rating } " +
+                 "FILTER restaurant.restaurant LIKE '%' ++ <str>$partialNameT ++ '%' OR restaurant.restaurant LIKE '%' ++ <str>$partialNameC ++ '%' OR  restaurant.restaurant LIKE '%' ++ <str>$partialNameS ++ '%';";
 
-      
+        }
+
         var result = await _edgeDbClient.QueryAsync<RestaurantGot>(query, new Dictionary<string, object>
         {
             { "city", city },
             { "partialNameC", partialNameC },
-             { "partialNameS", partialNameS }
+             { "partialNameS", partialNameS },
+                 { "partialNameT", partialNameT }
         });
         Restaurants = result.ToList();
         return Page();
